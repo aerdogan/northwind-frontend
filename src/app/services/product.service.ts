fbd16b0552425211349d2b926941f9d888a3576a
aerdogan <tuxposta@gmail.com>
2021-03-24 23:59:02 +0300

    login control, jwt token and guard activated

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
-import { ListResponseModel } from '../models/ListResponseModel';
+import { ListResponseModel } from '../models/responseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -21,13 +22,17 @@ export class ProductService {
 
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
     let newPath = this.apiUrl + "getlistbycategory?categoryId=" + categoryId;
-    console.log(newPath);
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 
   getById(productId:number):Observable<ListResponseModel<Product>> {
     let newPath = this.apiUrl + "getbyid?productId=" + productId;
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  add(product:Product):Observable<ResponseModel> {
+    let newPath = this.apiUrl+"add";
+    return this.httpClient.post<ResponseModel>(newPath, product);
   }
 
 }
